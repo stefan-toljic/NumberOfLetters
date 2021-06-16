97a92102f0e18746e17365f179e4daccfa2a129e
stefanToljic <85561921+stefan-toljic@users.noreply.github.com>
2021-06-16 12:22:33 +0000

    Reduced number of comparisons in if from 2 to 1.

--- main.py
+++ main.py
@@ -4,12 +4,12 @@
 Ask user to enter a sentence. Write number of occurrences of letter A in the string (both upper-case and lower-case letters should be counted).
 """
 
-user_input = input("Please enter a sentence:\n\t")
+user_input = input("Please enter a sentence:\n\t").lower()
 
 occurrences = 0
 
 for letter in user_input:
-    if letter == 'a' or letter == 'A':
+    if letter == 'a':
         occurrences += 1
 
 print(f"\n- The number of times letter 'A' appears is {occurrences}.")
