641d3826837c5f4ee6e9e54f65edfb5eb1956366
stefanToljic <85561921+stefan-toljic@users.noreply.github.com>
2021-06-16 12:21:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+2. Number of letters
+
+Ask user to enter a sentence. Write number of occurrences of letter A in the string (both upper-case and lower-case letters should be counted).
+"""
+
+user_input = input("Please enter a sentence:\n\t")
+
+occurrences = 0
+
+for letter in user_input:
+    if letter == 'a' or letter == 'A':
+        occurrences += 1
+
+print(f"\n- The number of times letter 'A' appears is {occurrences}.")
+
+# --------------------------------------------------------------------
